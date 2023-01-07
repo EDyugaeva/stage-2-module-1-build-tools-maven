088ba0238094783f58cd694da313d82eb1d19fef
EDyugaeva <ed2408@yandex.ru>
2023-01-07 11:42:41 +0500

    commit

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,20 @@
 package com.epam.demo;
 
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        boolean result = true;
+        for (String s :
+                args) {
+            int n = Integer.parseInt(s);
+            if (n > 0) {
+                result = true;
+            } else return false;
+        }
+
+        //magic happens here
+        return result;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,11 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        int n = Integer.parseInt(str);
+        if (n > 0) {
+            return true;
+        }
+
+        return false;
     }
 }
